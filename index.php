8c2b9d088e40457947e165871abccf91fdad888f
unknown <lfallamine26@gmail.com>
2024-12-17 16:41:00 +0000

    Second commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     // Fonction principale pour afficher le message
     function main() {
-        echo "Hello, GitHub!";
+        echo "Hello, M. LO! Comment vous allez?";
     }
     // Appel de la fonction principale
     main(); 
